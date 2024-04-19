39567ac040c00e9dd96825f183d190e16d819f09
Nizami Sevindi <nsevindi87@gmail.com>
2024-04-19 14:21:50 +0200

    Week03-Friday

--- Week03/Exercises/script.js
+++ Week03/Exercises/script.js
@@ -7,7 +7,8 @@ form.addEventListener("submit", function (e) {
 
   const userInput = document.getElementById("userNumber").value;
   let sortedNumber = sortedNumberFunc(userInput);
-  result.innerHTML = sortedNumber;
+  result.textContent = `Your sorted list is: [${sortedNumber}]`;
+  document.getElementById("userNumber").value = "";
 });
 
 function sortedNumberFunc(pStr) {
@@ -23,9 +24,11 @@ let sum = 0;
 
 //SUBMIT EVENT
 form02.addEventListener("submit", function (e) {
+  e.preventDefault();
   const userInput2 = parseInt(document.getElementById("userNumber02").value);
   let sumValue = handleSum(userInput2);
   result02.textContent = sumValue;
+  document.getElementById("userNumber02").value = "";
 });
 
 //ADD LOGIc
@@ -34,3 +37,28 @@ function handleSum(pNum) {
 }
 
 //EXAMPLE03
+const result03 = document.getElementById("result03");
+const form03 = document.getElementById("form03");
+let res;
+
+form03.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const userNumber03 = parseInt(document.getElementById("userNumber03").value);
+  const userNumber04 = parseInt(document.getElementById("userNumber04").value);
+  let result = handleSubstract(userNumber03, userNumber04);
+  result03.textContent = result;
+  document.getElementById("userNumber03").value = "";
+  document.getElementById("userNumber04").value = "";
+});
+
+function handleSubstract(pValue1, pValue2) {
+  if (pValue1 > pValue2) {
+    let res = pValue2 - pValue1;
+    return res;
+  } else if (pValue2 > pValue1) {
+    let res = pValue1 - pValue2;
+    return res;
+  } else {
+    return 0;
+  }
+}
